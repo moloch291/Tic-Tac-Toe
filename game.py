3c3628b0d17bfb868db076bfb925d0094e89eb63
moloch291 <danielkomjati1@gmail.com>
2020-07-16 17:09:26 +0200

    Win condition structure development process improved.

--- game.py
+++ game.py
@@ -3,6 +3,11 @@ def define_table():
     return table
 
 
+def win_conditions():
+    win_conditions_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    return win_conditions_list
+
+
 def get_player1_sign():
     player1_sign = input("Player 1: choose a sign! \nIt can be 'x' or 'o'... ").upper()
     if player1_sign == "x".upper():
@@ -14,7 +19,7 @@ def get_player1_sign():
         get_player1_sign()
 
 
-def get_player2_sign(player1_sign):
+def determine_player2_sign(player1_sign):
     if player1_sign == "x".upper():
         player2_sign = "O"
     else:
@@ -33,3 +38,11 @@ def set_mark_on_table(mark, table, sign):
         if table[i] == mark:
             table = table.replace(mark, sign)
     return table
+
+
+def win_check(win_conditions_list, mark):
+    for i in range(len(win_conditions_list)):
+        if mark in win_conditions_list[i]:
+            for w in range(len(win_conditions_list[i])):
+                if mark == w:
+
